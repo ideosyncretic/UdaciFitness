d5aaf97fca602cdfd37a7908a4f5f331d1921987
Angel Coco Chen <angel@nuisance.co>
2018-02-25 22:40:11 +0800

    feat: style steppers component

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import {
+  View,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  Platform,
+  StyleSheet
+} from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 import {
   getActivityMetaInfo,
@@ -13,11 +20,17 @@ import TextButton from './TextButton'
 import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
+import { white, purple } from '../utils/colors'
 
 const SubmitBtn = ({ onPress }) => {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>Submit</Text>
+    <TouchableOpacity
+      style={
+        Platform.OS === 'ios' ? styles.iosSubmitBtn : styles.androidSubmitBtn
+      }
+      onPress={onPress}
+    >
+      <Text style={styles.submitBtnText}>SUBMIT</Text>
     </TouchableOpacity>
   )
 }
@@ -109,23 +122,28 @@ class AddEntry extends Component {
 
     if (this.props.alreadyLogged) {
       return (
-        <View>
-          <Ionicons name="ios-happy-outline" size={100} />
+        <View style={styles.center}>
+          <Ionicons
+            name={Platform.OS === 'ios' ? 'ios-happy-outline' : 'md-happy'}
+            size={100}
+          />
           <Text>You already logged your information for today</Text>
-          <TextButton onPress={this.reset}>Reset</TextButton>
+          <TextButton style={{ padding: 10 }} onPress={this.reset}>
+            Reset
+          </TextButton>
         </View>
       )
     }
 
     return (
-      <View>
+      <ScrollView style={styles.container}>
         <DateHeader date={new Date().toLocaleDateString()} />
         {Object.keys(metaInfo).map(activity => {
           const { getIcon, type, ...rest } = metaInfo[activity]
           const value = this.state[activity]
 
           return (
-            <View key={activity}>
+            <View key={activity} style={styles.row}>
               {getIcon()}
               {type === 'slider' ? (
                 <UdaciSlider
@@ -145,10 +163,54 @@ class AddEntry extends Component {
           )
         })}
         <SubmitBtn onPress={this.submit} />
-      </View>
+      </ScrollView>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center'
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 30,
+    marginLeft: 30
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginRight: 40,
+    marginLeft: 40
+  },
+  androidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: 'center'
+  }
+})
 
 const mapStateToProps = state => {
   const key = timeToString()
