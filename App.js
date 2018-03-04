550626075941f9909289e133dbca6b7cb57cbe4e
Angel Coco Chen <angel@nuisance.co>
2018-03-04 16:30:15 +0800

    feat: show styled StatusBar at top of screen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Platform } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
 import { TabNavigator } from 'react-navigation'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import AddEntry from './components/AddEntry'
@@ -8,8 +8,17 @@ import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
 import { purple, white } from './utils/colors'
+import { Constants } from 'expo'
 
 const store = createStore(reducer)
+
+const UdaciStatusBar = ({ backgroundColor, ...props }) => {
+	return (
+		<View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+			<StatusBar translucent backgroundColor={backgroundColor} {...props} />
+		</View>
+	)
+}
 
 const Tabs = TabNavigator(
 	{
@@ -57,7 +66,7 @@ export default class App extends React.Component {
 		return (
 			<Provider store={store}>
 				<View style={{ flex: 1 }}>
-					<View style={{ height: 20 }} />
+					<UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
 					<Tabs />
 				</View>
 			</Provider>
