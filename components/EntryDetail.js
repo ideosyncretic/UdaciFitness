0a1efc7ebc618ba8dffce52951f2f053cf765f23
Angel Coco Chen <angel@nuisance.co>
2018-03-04 17:20:52 +0800

    feat: Display activities in detail on entry detail view

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import { white } from '../utils/colors'
+import ActivityCard from './ActivityCard'
 
 class EntryDetail extends Component {
+	static navigationOptions = ({ navigation }) => {
+		const { entryId } = navigation.state.params
+
+		const year = entryId.slice(0, 4)
+		const month = entryId.slice(5, 7)
+		const day = entryId.slice(8)
+		return {
+			title: `${day}/${month}/${year}`,
+		}
+	}
+
 	render() {
+		const { activities } = this.props
 		return (
-			<View>
+			<View style={styles.container}>
+				<ActivityCard activities={activities} />
 				<Text>Entry Detail - {this.props.navigation.state.params.entryId}</Text>
 			</View>
 		)
 	}
 }
-export default EntryDetail
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: white,
+		padding: 15,
+	},
+})
+
+const mapStateToProps = (state, { navigation }) => {
+	const { entryId } = navigation.state.params
+
+	return {
+		entryId,
+		activities: state[entryId],
+	}
+}
+
+export default connect(mapStateToProps)(EntryDetail)
